cee0923fe557ecec8f34209bb41104a4eb5fc68a
Syahrizal As <alisadikinsyahrizal@gmail.com>
2024-12-17 12:00:48 +0700

    update edit

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -47,6 +47,11 @@ export default function AuthenticatedLayout({ header, children }) {
                                         Roles
                                     </NavLink>
                                 )}
+                                {hasAnyPermission(['users index']) &&
+                                    <NavLink href={route('users.index')} active={route().current('users*')}>
+                                        Users
+                                    </NavLink>
+                                }
                             </div>
                         </div>
 
@@ -168,6 +173,12 @@ export default function AuthenticatedLayout({ header, children }) {
                                 Roles
                             </ResponsiveNavLink>
                         )}
+
+                        {hasAnyPermission(['users index']) &&
+                            <ResponsiveNavLink href={route('users.index')} active={route().current('users*')}>
+                                Users
+                            </ResponsiveNavLink>
+                        }
                     </div>
 
                     <div className="border-t border-gray-200 pb-1 pt-4">
